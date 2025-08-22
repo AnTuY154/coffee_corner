ca10e6bc202639f20a4ff423d86333a2905cbea2
Đỗ Trọng Anh Tuấn <55184774+AnTuY154@users.noreply.github.com>
2025-08-22 17:31:27 +0700

    Update send-mail.ts

--- pages/api/send-mail.ts
+++ pages/api/send-mail.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     },
   });
   
-  console.log('Email sent successfully', process.env.GMAIL_USER);
+  console.log('Email sent successfully');
   
   try {
     transporter.sendMail({
@@ -48,7 +48,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       'Ghi chú': note || '',
     });
 
-    return res.status(200).json({ message: 'Email sent successfully' });
+    return res.status(200).json({ message: 'Email sent successfully' + process.env.GMAIL_USER });
   } catch (error) {
     return res.status(500).json({ message: 'Failed to send email', error });
   }
